065b0c82994f3ff03592bba89d60edd501e2509e
Diego Fernández Criado <diegofercri25@gmail.com>
2024-04-05 08:54:25 +0200

    js and css navbar dropdown rm

--- js/navbar.js
+++ js/navbar.js
@@ -8,30 +8,13 @@ toggleMenuOpen.addEventListener('click', e => {
 
 const elements = document.querySelectorAll('.toggle-menu__close');
 elements.forEach(element => {
-  element.addEventListener('click', () => {
-    navbar2.classList.toggle('navbar2--show');
-    mainMenu.classList.toggle('main-menu--show');
-  });
+    element.addEventListener('click', () => {
+        navbar2.classList.toggle('navbar2--show');
+        mainMenu.classList.toggle('main-menu--show');
+    });
 });
 
-
-function imgCloseChecker() {
-    // Verifica si ambos toggles están activados
-    if (dropdown2List0.classList.contains('dropdown2-list--show') || dropdown2List1.classList.contains('dropdown2-list--show-1')) {
-        // Asegura que la clase esté presente para ocultar la imagen
-        navbar2.classList.add('navbar2-logo--hidden');
-        setTimeout(function () {
-            navbar2.classList.add('navbar2-logo--hidden2');
-        }, 500)
-    } else {
-        // Asegura que la clase esté ausente para mostrar la imagen
-        navbar2.classList.remove('navbar2-logo--hidden');
-        navbar2.classList.remove('navbar2-logo--hidden2');
-    }
-}
-
-
-window.addEventListener('resize',() => {
+window.addEventListener('resize', () => {
     if (navbar2.classList.contains('navbar2--show')) {
         navbar2.classList.remove('navbar2--show');
         mainMenu.classList.remove('main-menu--show');
